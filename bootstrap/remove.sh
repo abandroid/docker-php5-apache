b6c8554643c45b0c92f3774702545800e25f0034
endroid <info@endroid.nl>
2016-01-13 14:21:29 +0100

    Improve shell commands

--- bootstrap/remove.sh
+++ bootstrap/remove.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+docker-compose rm
 docker stop $(docker ps -a -q) > /dev/null
 docker rm -f $(docker ps -a -q) > /dev/null
 docker rmi -f $(docker images -q) > /dev/null
--- bootstrap/run.sh
+++ bootstrap/run.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-docker-compose pull
 docker-compose build
 docker-compose up -d
 docker exec -t -i -u 1000 bootstrap_php_1 /bin/bash
