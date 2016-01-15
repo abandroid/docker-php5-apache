c3ce60a841f0a2d854833ae6072e7de00dfca27c
endroid <info@endroid.nl>
2016-01-15 22:28:48 +0100

    Remove containers after stopping

--- bootstrap/remove.sh
+++ bootstrap/remove.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker-compose rm
 docker stop $(docker ps -a -q) > /dev/null
+docker-compose rm
 docker rm -f $(docker ps -a -q) > /dev/null
 docker rmi -f $(docker images -q) > /dev/null
