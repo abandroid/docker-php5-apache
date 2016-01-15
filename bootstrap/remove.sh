e4277bb941dc18e9e1c5619de922540e3537a095
endroid <info@endroid.nl>
2016-01-15 23:28:23 +0100

    Hide shell script output

--- bootstrap/remove.sh
+++ bootstrap/remove.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sh stop.sh
-docker rm -f $(docker ps -a -q) > /dev/null
-docker rmi -f $(docker images -q) > /dev/null
+sh stop.sh > /dev/null 2>&1
+docker rm -f $(docker ps -a -q) > /dev/null 2>&1
+docker rmi -f $(docker images -q) > /dev/null 2>&1
--- bootstrap/stop.sh
+++ bootstrap/stop.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker stop $(docker ps -a -q) > /dev/null
+docker stop $(docker ps -a -q) > /dev/null 2>&1
