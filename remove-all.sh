d7398f976fc46df74215ca639fa93d66ef1bf91f
endroid <info@endroid.nl>
2016-03-09 15:59:06 +0100

    Update directory structure

--- remove-all.sh
+++ remove-all.sh
@@ -3,3 +3,4 @@
 sh stop.sh
 docker rm -f $(docker ps -a -q) > /dev/null 2>&1
 docker rmi -f $(docker images -q) > /dev/null 2>&1
+docker volume rm $(docker volume ls -qf dangling=true) > /dev/null 2>&1
